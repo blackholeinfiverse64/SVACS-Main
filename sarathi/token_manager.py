f5cd9373ab9a8cea8a4881535519c90d4d93c683
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-05-11 13:34:59 +0530

    Create token_manager.py

---
+++ sarathi/token_manager.py
@@ -0,0 +1,51 @@
+import secrets
+
+
+# =========================================================
+# GENERATE TOKEN
+# =========================================================
+
+def generate_token(execution_request):
+
+    token = secrets.token_hex(16)
+
+    return {
+        "sarathi_token": {
+            "token": token
+        }
+    }
+
+
+# =========================================================
+# VALIDATE TOKEN
+# =========================================================
+
+def validate_token(execution_request):
+
+    # -----------------------------------------------------
+    # safe extraction
+    # -----------------------------------------------------
+
+    sarathi_token = execution_request.get(
+        "sarathi_token",
+        {}
+    )
+
+    token = sarathi_token.get(
+        "token"
+    )
+
+    # -----------------------------------------------------
+    # validation
+    # -----------------------------------------------------
+
+    if (
+        not token
+        or token == "fake_token"
+    ):
+
+        raise Exception(
+            "Invalid token detected"
+        )
+
+    return True
