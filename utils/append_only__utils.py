23b75d839fcf459133d404945190b58bb9f539aa
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-05-11 13:46:10 +0530

    Create append_only__utils.py

---
+++ utils/append_only__utils.py
@@ -0,0 +1,12 @@
+import hashlib
+import json
+
+
+def calculate_hash(data):
+
+    return hashlib.sha256(
+        json.dumps(
+            data,
+            sort_keys=True
+        ).encode()
+    ).hexdigest()
