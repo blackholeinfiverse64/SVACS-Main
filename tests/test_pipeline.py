6ca41a5ce01e11edda7e32317db3662a3d8bcb7a
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-05-18 19:24:36 +0530

    test_pipeline.py

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -6,7 +6,7 @@ import os
 # =========================================================
 
 sys.path.append(
-    os.path.abspath(
+    os.path.abspath( 
         os.path.join(
             os.path.dirname(__file__),
             '..'
