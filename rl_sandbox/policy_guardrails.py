a510a2e702ea611f4b3d34822444cf136547e021
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-05-26 08:52:49 +0530

    Create policy_guardrails.py

---
+++ rl_sandbox/policy_guardrails.py
@@ -0,0 +1,9 @@
+restricted = [
+    "governance_semantics",
+    "contract_meaning",
+    "execution_authority",
+    "replay_truth",
+    "constitutional_boundaries"
+]
+
+print("RL_BOUNDARIES_ACTIVE")
