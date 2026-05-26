c3d79d1b4073e98a8cfa98421415f4920e30ee76
blackholeinfiverse116-ship-it <blackholeinfiverse116@gmail.com>
2026-05-26 08:27:37 +0530

    Create geo_provenance_validator.py

---
+++ geo/geo_provenance_validator.py
@@ -0,0 +1,23 @@
+import json
+
+with open("geo/geo_provenance_schema.json") as f:
+    data = json.load(f)
+
+required = [
+    "geo_source_lineage",
+    "coordinate_origin",
+    "sensor_origin",
+    "spatial_uncertainty",
+    "timestamp_uncertainty",
+    "coordinate_confidence",
+    "geo_transformation_history",
+    "location_reference_version",
+    "geo_validation_status"
+]
+
+valid = all(field in data for field in required)
+
+if valid:
+    print("GEO_PROVENANCE_VALID")
+else:
+    print("GEO_PROVENANCE_INVALID")
